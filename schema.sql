fe853f801719cfefadb3bc2219a153f4f7262ef1
AlyAtalla <101482711+AlyAtalla@users.noreply.github.com>
2023-09-20 02:41:58 +0300

    Add tables in schema

--- schema.sql
+++ schema.sql
@@ -11,9 +11,42 @@ CREATE TABLE species (
     name VARCHAR(255)
 );
 
--- Modify the 'animals' table
-ALTER TABLE animals
-    ADD COLUMN species_id INT,
-    ADD COLUMN owner_id INT,
-    ADD CONSTRAINT fk_species FOREIGN KEY (species_id) REFERENCES species(id),
-    ADD CONSTRAINT fk_owner FOREIGN KEY (owner_id) REFERENCES owners(id);
+-- Create the 'animals' table
+CREATE TABLE animals (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(255),
+    date_of_birth DATE,
+    escape_attempts INT,
+    neutered BOOLEAN,
+    weight_kg DECIMAL(5, 2),
+    species_id INT,
+    owner_id INT,
+    FOREIGN KEY (species_id) REFERENCES species(id),
+    FOREIGN KEY (owner_id) REFERENCES owners(id)
+);
+
+-- Create the 'vets' table
+CREATE TABLE vets (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(255),
+    age INT,
+    date_of_graduation DATE
+);
+
+-- Create the 'specializations' table (for the many-to-many relationship between species and vets)
+CREATE TABLE specializations (
+    id SERIAL PRIMARY KEY,
+    vet_id INT,
+    species_name VARCHAR(255),
+    FOREIGN KEY (vet_id) REFERENCES vets(id)
+);
+
+-- Create the 'visits' table (for the many-to-many relationship between animals and vets)
+CREATE TABLE visits (
+    id SERIAL PRIMARY KEY,
+    animal_id INT,
+    vet_id INT,
+    visit_date DATE,
+    FOREIGN KEY (animal_id) REFERENCES animals(id),
+    FOREIGN KEY (vet_id) REFERENCES vets(id)
+);
