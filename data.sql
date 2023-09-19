5b8ef525735fe3478e3a6be985120c5cf9184534
AlyAtalla <101482711+AlyAtalla@users.noreply.github.com>
2023-09-20 02:42:26 +0300

    Insert data

--- data.sql
+++ data.sql
@@ -12,17 +12,39 @@ INSERT INTO species (name) VALUES
     ('Pokemon'),
     ('Digimon');
 
--- Modify animals based on name and assign species_id
-UPDATE animals SET species_id = (CASE
-    WHEN name LIKE '%mon' THEN (SELECT id FROM species WHERE name = 'Digimon')
-    ELSE (SELECT id FROM species WHERE name = 'Pokemon')
-END);
+-- Insert data into the 'vets' table
+INSERT INTO vets (name, age, date_of_graduation) VALUES
+    ('Vet William Tatcher', 45, '2000-04-23'),
+    ('Vet Maisy Smith', 26, '2019-01-17'),
+    ('Vet Stephanie Mendez', 64, '1981-05-04'),
+    ('Vet Jack Harkness', 38, '2008-06-08');
 
--- Modify animals and assign owner_id
-UPDATE animals SET owner_id = (CASE
-    WHEN name IN ('Agumon') THEN (SELECT id FROM owners WHERE full_name = 'Sam Smith')
-    WHEN name IN ('Gabumon', 'Pikachu') THEN (SELECT id FROM owners WHERE full_name = 'Jennifer Orwell')
-    WHEN name IN ('Devimon', 'Plantmon') THEN (SELECT id FROM owners WHERE full_name = 'Bob')
-    WHEN name IN ('Charmander', 'Squirtle', 'Blossom') THEN (SELECT id FROM owners WHERE full_name = 'Melody Pond')
-    WHEN name IN ('Angemon', 'Boarmon') THEN (SELECT id FROM owners WHERE full_name = 'Dean Winchester')
-END);
+-- Insert data into the 'specializations' table
+INSERT INTO specializations (vet_id, species_name) VALUES
+    (1, 'Pokemon'),
+    (3, 'Digimon'),
+    (3, 'Pokemon'),
+    (4, 'Digimon');
+
+-- Insert data into the 'visits' table
+INSERT INTO visits (animal_id, vet_id, visit_date) VALUES
+    (1, 1, '2020-05-24'),
+    (1, 3, '2020-07-22'),
+    (2, 4, '2021-02-02'),
+    (3, 2, '2020-01-05'),
+    (3, 2, '2020-03-08'),
+    (3, 2, '2020-05-14'),
+    (4, 3, '2021-05-04'),
+    (5, 4, '2021-02-24'),
+    (6, 2, '2019-12-21'),
+    (6, 1, '2020-08-10'),
+    (6, 2, '2021-04-07'),
+    (7, 3, '2019-09-29'),
+    (8, 4, '2020-10-03'),
+    (8, 4, '2020-11-04'),
+    (9, 2, '2019-01-24'),
+    (9, 2, '2019-05-15'),
+    (9, 2, '2020-02-27'),
+    (9, 2, '2020-08-03'),
+    (10, 3, '2020-05-24'),
+    (11, 1, '2021-01-11');
