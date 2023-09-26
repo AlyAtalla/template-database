1a07a04ea6a4fc81079900a3f54f3574612ad19e
AlyAtalla <101482711+AlyAtalla@users.noreply.github.com>
2023-09-26 13:24:06 +0300

    Add index invoices medhist id on invoices

--- schema_based_on_diagram.sql
+++ schema_based_on_diagram.sql
@@ -54,3 +54,5 @@ CREATE INDEX idx_treatment_id ON medical_history_treatments (treatment_id);
 CREATE INDEX idx_invoice_items_invoice_id ON invoice_items (invoice_id);
 
 CREATE INDEX idx_invoice_items_treatment_id ON invoice_items (treatment_id);
+
+CREATE INDEX idx_invoices_medicalhistory_id ON invoices (medical_history_id);
