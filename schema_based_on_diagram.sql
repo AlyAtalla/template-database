2791b1ef4e9af43416913832d8848b0fe88104af
AlyAtalla <101482711+AlyAtalla@users.noreply.github.com>
2023-09-26 12:53:00 +0300

    Create invoice table

--- schema_based_on_diagram.sql
+++ schema_based_on_diagram.sql
@@ -6,3 +6,12 @@
   patient_id INT NOT NULL,
   status VARCHAR(150)
 );
+
+CREATE TABLE invoices (
+  id SERIAL PRIMARY KEY,
+  total_amount DECIMAL,
+  generated_at TIMESTAMP,
+  payed_at TIMESTAMP,
+  medical_history_id INT,
+  FOREIGN KEY (medical_history_id) REFERENCES medical_histories (id)
+);
