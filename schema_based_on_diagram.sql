97d2f439bf3a3694bfa9c2857cef142e66823ee1
WILSAN NJI <wilsanfourrspeed@gmail.com>
2023-09-26 11:20:03 +0100

    Create index for tretment id on invoice items table

--- schema_based_on_diagram.sql
+++ schema_based_on_diagram.sql
@@ -52,3 +52,5 @@ CREATE INDEX idx_medhist_id ON medical_history_treatments (medical_history_id);
 CREATE INDEX idx_treatment_id ON medical_history_treatments (treatment_id);
 
 CREATE INDEX idx_invoice_items_invoice_id ON invoice_items (invoice_id);
+
+CREATE INDEX idx_invoice_items_treatment_id ON invoice_items (treatment_id);
